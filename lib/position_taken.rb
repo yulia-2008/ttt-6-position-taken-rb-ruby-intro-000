c64e497347b760b946e44539b0977dea4fc01fea
Yuliya Yurkevich <juliana.ny2008@gmail.com>
2020-04-14 14:35:22 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,7 +2,9 @@
 def position_taken?(board, index)
   if board[index] != " " || board[index] != "" || board[index] != nil
   return false
-elsif board[index]
+elsif board[index] =="X" || board[index] =="O"
+  return true
+else 
   return true
 end
 end
