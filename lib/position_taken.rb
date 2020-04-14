aead01bd03eb02043cab74e393d7361fd8ba0761
Yuliya Yurkevich <juliana.ny2008@gmail.com>
2020-04-14 15:00:45 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,10 +1,6 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
-  if board[index] != " " || board[index] != "" || board[index] != nil
-  return false
-elsif board[index] =="X" || board[index] =="O"
-  return true
-else 
-  return true
-end
-end
+   board[index] != " " || board[index] != "" || board[index] != nil
+ 
+ board[index] =="X" || board[index] =="O"
+ end
